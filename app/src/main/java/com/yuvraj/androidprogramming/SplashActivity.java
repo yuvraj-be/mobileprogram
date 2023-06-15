2f32feb600403c8d605fff14d2c0e1055e8af6fa
Yuvraj Belbase <ashritbelbase@gmail.com>
2023-06-15 13:09:20 +0545

    splash

--- app/src/main/java/com/yuvraj/androidprogramming/SplashActivity.java
+++ app/src/main/java/com/yuvraj/androidprogramming/SplashActivity.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-import com.app.superpos.login.LoginActivity;
+import com.yuvraj.androidprogramming.login.LoginActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
